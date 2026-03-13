1bb2982880e8c1b8f9d29ba2670774f0178c32c1
samueldb2002 <sam.doorenbosch@gmail.com>
2026-03-13 13:39:47 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3002;
+const DIR = __dirname;
+
+const mime = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(DIR, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
